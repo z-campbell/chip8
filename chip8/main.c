52c4a49643b46d0a38697d191ecae0278c21be82
Zach Campbell <zcampbell@ddi.local>
2018-05-28 12:23:34 -0700

    Initial Commit

---
+++ chip8/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  chip8
+//
+//  Created by Zach Campbell on 5/28/18.
+//  Copyright © 2018 Zach Campbell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
